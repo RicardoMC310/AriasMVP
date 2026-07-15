78876eae89511568a3f0871bc55b41baf5ed51fa
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 16:07:39 -0300

    feat: accept db parameter in main router and pass to user router

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, type Router } from "express";
 import createHttpResponse from "./platform/express/create-response.express.js";
 import makeUserRouter from "./modules/user/presentation/controller/user.router.js";
+import { Kysely } from "kysely";
+import { DB } from "./platform/database/db.js";
 
-export default function makeRouter(): Router {
+export default function makeRouter(db: Kysely<DB>): Router {
     const router: Router = express.Router();
 
     router.get("/healthy", (_req: Request, res: Response) => {
@@ -10,7 +12,7 @@ export default function makeRouter(): Router {
         res.status(200).json(response);
     });
 
-    const userRouter = makeUserRouter();
+    const userRouter = makeUserRouter(db);
 
     router.use(userRouter.prefix, userRouter.router);
 
