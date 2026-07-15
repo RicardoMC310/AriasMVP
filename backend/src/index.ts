3211b7f033091a648aad4880f61dd75d3721e572
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 14:04:34 -0300

    feat: restructure server entry point with dotenv, router, and error middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,21 @@
-import express, { type Express, type Request, type Response } from "express";
+import express, { Express } from "express"
+import { config as dotenvConfig } from "dotenv";
+import makeRouter from "./router.js";
+import errorMiddleware from "./platform/express/middleware/error.middleware.js";
+dotenvConfig();
 
-const app: Express = express();
+(async () => {
+    const app: Express = express();
 
-app.get("/healthy", (_req: Request, res: Response) => {
-    res.status(200).json({
-        message: "OK",
-        status: 200
+    app.use(express.json());
+    app.use(makeRouter());
+
+    const host = process.env.SERVER_HOST ?? "0.0.0.0";
+    const port = Number(process.env.SERVER_PORT ?? 8080);
+
+    app.use(errorMiddleware);
+
+    app.listen(port, host, () => {
+        console.log(`Running server in http://${host}:${port}`);
     });
-});
+})()
