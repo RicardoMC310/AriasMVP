0e7df9d8f0fbce9b41f8dadf6a6505a469180792
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 16:07:23 -0300

    feat: add unit tests for invalid email and password validation in register use case

--- backend/src/modules/user/application/use-cases/register/register.use-case.unit.test.ts
+++ backend/src/modules/user/application/use-cases/register/register.use-case.unit.test.ts
@@ -1,8 +1,20 @@
+import InvalidEmailException from "../../../../../core/domain/exception/invalid-email.exception.js";
 import UserEntity from "../../../domain/entity/user.entity.js";
+import InvalidPasswordException from "../../exception/invalid-password.exception.js";
 import IUserHasher from "../port/hasher.port.js";
 import RegisterUserUseCase from "./register.use-case.js";
 
 describe("User registration use case tests", () => {
+
+    let registerUseCase: RegisterUserUseCase;
+    let testUserRepository: TestFakeUserRepository;
+    let testUserHasher: TestFakeUserHasher;
+
+    beforeEach(() => {
+        testUserRepository = new TestFakeUserRepository();
+        testUserHasher = new TestFakeUserHasher();
+        registerUseCase = new RegisterUserUseCase(testUserRepository, testUserHasher);
+    });
 
     it("It should register a user without issues", async () => {
         const body = {
@@ -11,19 +23,39 @@ describe("User registration use case tests", () => {
             password: "Rm30042009#"
         };
 
-        const testRepository = new TestFakeUserRepository();
-        const testHasher = new TestFakeUserHasher();
-        const userCase = new RegisterUserUseCase(testRepository, testHasher);
-
-        await expect(userCase.execute(body))
+        await expect(registerUseCase.execute(body))
             .resolves
             .not
             .toThrow();
 
-        expect(testRepository.users).toHaveLength(1);
-        expect(testRepository.users[0].username).toBe(body.username);
-        expect(testRepository.users[0].email).toBe(body.email);
-        expect(testRepository.users[0].passwordHash).toBe("hashed:" + body.password);
+        expect(testUserRepository.users).toHaveLength(1);
+        expect(testUserRepository.users[0].username).toBe(body.username);
+        expect(testUserRepository.users[0].email).toBe(body.email);
+        expect(testUserRepository.users[0].passwordHash).toBe("hashed:" + body.password);
+    });
+
+    it("It should throw an exception if the email is invalid", async () => {
+        const body = {
+            username: "ricardo",
+            email: "ricardo",
+            password: "Rm30042009#"
+        };
+
+        await expect(registerUseCase.execute(body)).rejects.toThrow(InvalidEmailException);
+
+        expect(testUserRepository.users).toHaveLength(0);
+    });
+
+    it("It should throw an exception if the password is invalid", async () => {
+        const body = {
+            username: "ricardo",
+            email: "ricardo@gmain.com",
+            password: "Rm30"
+        };
+
+        await expect(registerUseCase.execute(body)).rejects.toThrow(InvalidPasswordException);
+
+        expect(testUserRepository.users).toHaveLength(0);
     });
 
 });
