ba57f61a57c5cfc8f8c5c75dfc0a17837a80e2a3
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 16:07:43 -0300

    feat: initialize database connection at server startup and inject into router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,20 @@ import express, { Express } from "express"
 import { config as dotenvConfig } from "dotenv";
 import makeRouter from "./router.js";
 import errorMiddleware from "./platform/express/middleware/error.middleware.js";
+import { createDatabase } from "./platform/database/kysely.connection.js";
 dotenvConfig();
 
 (async () => {
+    
+    if (process.env.DATABASE_URL === undefined)
+        throw new Error("Missing environment variable DATABASE_URL");
+
+    const db = createDatabase(process.env.DATABASE_URL);
+
     const app: Express = express();
 
     app.use(express.json());
-    app.use(makeRouter());
+    app.use(makeRouter(db));
 
     const host = process.env.SERVER_HOST ?? "0.0.0.0";
     const port = Number(process.env.SERVER_PORT ?? 8080);
