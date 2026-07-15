f2b777a3be4412c38d9a78ea84899fcfab449074
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 14:03:57 -0300

    chore: add kysely-ctl configuration for database migrations

---
+++ backend/.config/kysely.config.ts
@@ -0,0 +1,20 @@
+import { PostgresDialect } from 'kysely';
+import { defineConfig } from 'kysely-ctl';
+import { Pool } from "pg";
+
+export default defineConfig({
+	// replace me with a real dialect instance OR a dialect name + `dialectConfig` prop.
+	dialect: new PostgresDialect({
+		pool: new Pool({
+			connectionString: process.env.DATABASE_URL
+		})
+	}),
+		
+	migrations: {
+		migrationFolder: "../src/platform/database/migrations",
+	}
+	//   plugins: [],
+	//   seeds: {
+	//     seedFolder: "seeds",
+	//   }
+});
