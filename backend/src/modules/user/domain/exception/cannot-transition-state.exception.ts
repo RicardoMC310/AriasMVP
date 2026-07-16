39c8c72394a28bdaaa48de0299f58cb83fabfded
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 11:10:48 -0300

    feat: add exception for invalid user state transitions

---
+++ backend/src/modules/user/domain/exception/cannot-transition-state.exception.ts
@@ -0,0 +1,13 @@
+import DomainException, { DetailError } from "../../../../core/domain/exception/domain.exception.js"
+import { UserState } from "../entity/user.entity.js";
+
+export default class UserCannotTransitionStateException extends DomainException {
+
+    constructor(current: UserState, from: UserState) {
+        super(
+            "User cannot transition state from " + current as string + " to " + from as string,
+            DetailError.AUTHORIZATION
+        );
+    }
+
+}
