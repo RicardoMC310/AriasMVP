2f9f73aef74efde9b2fa34e9ff330adb250f8f67
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 22:15:50 -0300

    feat: add findUserByEmail method to user repository interface

--- backend/src/modules/user/domain/repository/user.repository.ts
+++ backend/src/modules/user/domain/repository/user.repository.ts
@@ -2,4 +2,5 @@ import UserEntity from "../entity/user.entity.js";
 
 export default interface IUserRepository {
     save(userEntity: UserEntity): Promise<void>;
+    findUserByEmail(email: string): Promise<UserEntity | null>;
 }
