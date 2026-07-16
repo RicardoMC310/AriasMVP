423c2a1b0c677d812eb96dbf0eaeba921e9c4af9
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 09:47:39 -0300

    test: assert user entity state property in unit tests

--- backend/src/modules/user/domain/entity/user.entity.unit.test.ts
+++ backend/src/modules/user/domain/entity/user.entity.unit.test.ts
@@ -1,5 +1,6 @@
 import UserEntityBuilder from "../builder/user-entity.builder.js";
 import { describe, expect, it } from "@jest/globals";
+import { UserState } from "./user.entity.js";
 
 describe("Testes da entidade de usuário", () => {
 
@@ -12,12 +13,14 @@ describe("Testes da entidade de usuário", () => {
             .withUsername(username)
             .withEmail(email)
             .withPasswordHash(passwordHash)
+            .withState(UserState.ACTIVE)
             .build();
 
         expect(userEntity.id).toBeNull();
         expect(userEntity.email).toBe(email);
         expect(userEntity.username).toBe(username);
         expect(userEntity.passwordHash).toBe(passwordHash);
+        expect(userEntity.state).toBe(UserState.ACTIVE);
     });
 
 });
