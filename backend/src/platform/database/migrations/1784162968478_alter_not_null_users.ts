f1857a1f54f5ac2004aac8b6086c864aca5f9794
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 22:15:24 -0300

    feat: add refresh_token migration and alter users not null constraints

---
+++ backend/src/platform/database/migrations/1784162968478_alter_not_null_users.ts
@@ -0,0 +1,18 @@
+import type { Kysely } from 'kysely'
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema
+		.alterTable("users")
+		.alterColumn("email", col => col.setNotNull())
+		.alterColumn("password_hash", col => col.setNotNull())
+		.alterColumn("name", col => col.setNotNull())
+		.execute();
+}
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function down(db: Kysely<any>): Promise<void> {
+	// down migration code goes here...
+	// note: down migrations are optional. you can safely delete this function.
+	// For more info, see: https://kysely.dev/docs/migrations
+}
