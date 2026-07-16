02e26ed76081dded2a6f7945ab691d1c5ea7bd71
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-07-16 19:44:54 -0300

    feat: add inputs and controllers in Auth Screens

--- app/lib/screens/auth/login/index.dart
+++ app/lib/screens/auth/login/index.dart
@@ -1,18 +1,46 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController(); 
+  final TextEditingController passwordController = TextEditingController(); 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: () => context.go('/register'), child: Text("Don't have an account? Register")),
-        ],
-      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              key: Key('email'),
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: 'E-mail'
+              ),
+            ),
+            TextField(
+              key: Key('password'),
+              controller: passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password'
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () => {}, child: Text('Login')),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () => context.go('/register'), child: Text("Don't have an account? Register")),
+          ],
+        ),
+      )
     );
   }
 }
--- app/lib/screens/auth/register/index.dart
+++ app/lib/screens/auth/register/index.dart
@@ -1,18 +1,54 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController nameController = TextEditingController(); 
+  final TextEditingController emailController = TextEditingController(); 
+  final TextEditingController passwordController = TextEditingController(); 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Register')),
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: () => context.go('/login'), child: Text('Already have an account? Login')),
-        ],
-      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              key: Key('name'),
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Name'
+              ),
+            ),
+            TextField(
+              key: Key('email'),
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: 'E-mail'
+              ),
+            ),
+            TextField(
+              key: Key('password'),
+              controller: passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password'
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () => {}, child: Text('Register')),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () => context.go('/login'), child: Text("Already have an account? Login")),
+          ],
+        ),
+      )
     );
   }
 }
