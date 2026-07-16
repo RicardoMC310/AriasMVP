8d0a1609bb226d29191c6264f18100fd682bbf58
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 22:16:53 -0300

    feat: register auth router in main router

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -3,6 +3,7 @@ import createHttpResponse from "./platform/express/create-response.express.js";
 import makeUserRouter from "./modules/user/presentation/controller/user.router.js";
 import { Kysely } from "kysely";
 import { DB } from "./platform/database/db.js";
+import makeAuthRouter from "./modules/auth/presentation/controller/auth.router.js";
 
 export default function makeRouter(db: Kysely<DB>): Router {
     const router: Router = express.Router();
@@ -13,8 +14,10 @@ export default function makeRouter(db: Kysely<DB>): Router {
     });
 
     const userRouter = makeUserRouter(db);
+    const authRouter = makeAuthRouter(db);
 
     router.use(userRouter.prefix, userRouter.router);
+    router.use(authRouter.prefix, authRouter.router);
 
     return router;
 }
