ccb467773a5a4baecd1f8adfd690209122e33e78
VitaoDeveloper <paulo.borges.prates@gmail.com>
2026-07-15 23:38:35 -0300

    feat: create default screens

---
+++ app/lib/screens/landing/index.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Landing extends StatelessWidget {
+  const Landing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Landing Screen')),
+    );
+  }
+}
