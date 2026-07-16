96dd47e1e93d016213c58959145c4a975e443649
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 09:47:10 -0300

    feat: add UserState enum and state property to user entity

--- backend/src/modules/user/domain/entity/user.entity.ts
+++ backend/src/modules/user/domain/entity/user.entity.ts
@@ -1,12 +1,19 @@
 import Email from "../../../../core/domain/vo/email.vo.js";
 
+export enum UserState {
+    VERIFICATION_PENDING = "VERIFICATION_PENDING",
+    ACTIVE = "ACTIVE",
+    BLOCKED = "BLOCKED"
+};
+
 export default class UserEntity {
 
     constructor(
         private _username: string,
         private _email: Email,
         private _passwordHash: string,
-        private _id: string | null = null
+        private _id: string | null = null,
+        private _state: UserState = UserState.VERIFICATION_PENDING
     ) {}
 
     get username(): string {
@@ -23,6 +30,10 @@ export default class UserEntity {
 
     get id(): string | null {
         return this._id;
+    }
+
+    get state(): UserState {
+        return this._state;
     }
 
 }
