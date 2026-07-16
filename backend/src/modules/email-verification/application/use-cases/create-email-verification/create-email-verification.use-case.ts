ef49c4fd6cc86561a8c223b1d4a40eb08d798800
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 17:09:57 -0300

    feat: add create email verification use case

---
+++ backend/src/modules/email-verification/application/use-cases/create-email-verification/create-email-verification.use-case.ts
@@ -0,0 +1,43 @@
+import Email from "../../../../../core/domain/vo/email.vo.js";
+import EmailVerificationEntityBuilder from "../../../domain/builder/email-verification.builder.js";
+import CreateEmailVerificationDTO from "../../dto/in/create-email-verification/create-email-verification.dto.js";
+import { v7 as uuidv7 } from "uuid";
+import IEmailVerificationCodeGenerator from "../../port/code-generator.port.js";
+import IEmailVerificationRepository from "../../../domain/repository/email-verification.repository.js";
+
+export default class CreateEmailVerificationUseCase {
+
+    private readonly MILLISECONDS = 1000;
+    private readonly SECONDS = 60;
+    private readonly MINUTES = 30;
+    private readonly TOKEN_LENGTH = 8;
+
+    constructor(
+        private readonly codeGenerator: IEmailVerificationCodeGenerator,
+        private readonly repository: IEmailVerificationRepository
+    ) {}
+
+    async execute(dto: CreateEmailVerificationDTO): Promise<void> {
+        this.validate(dto);
+
+        const expiresAt = new Date(
+            Date.now() + this.MILLISECONDS * this.SECONDS * this.MINUTES
+        );
+
+        const { token, hash } = this.codeGenerator.generator(this.TOKEN_LENGTH);
+
+        const emailVerificationEntity = EmailVerificationEntityBuilder.create()
+            .withId(uuidv7())
+            .withEmail(dto.email)
+            .withUserId(dto.userId)
+            .withExpiresAt(expiresAt)
+            .withCodeHash(hash)
+            .build();
+
+        await this.repository.save(emailVerificationEntity);
+    }
+
+    private validate(dto: CreateEmailVerificationDTO) {
+        Email.isValid(dto.email);
+    }
+}
