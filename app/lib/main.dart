1a3ea6bdac6ef4aa9b1c21abfedb727539c351b5
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-07-16 17:27:39 -0300

    feat: add provider scope in application bootstrap

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'router/router.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp()
+    )
+  );
 }
 
 class MainApp extends StatelessWidget {
