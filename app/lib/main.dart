265cf69e8a4163b2790e97968b054c65d20bd63b
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-07-16 18:37:01 -0300

    feat: create MainAppState in main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app/global/states/auth_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'router/router.dart';
@@ -10,11 +11,23 @@ void main() {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerStatefulWidget {
   const MainApp({super.key});
 
   @override
+  ConsumerState<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends ConsumerState<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() => ref.read(authProvider.notifier).init());
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final router = ref.watch(routerProvider);
     return MaterialApp.router(
       routerConfig: router,
     );
