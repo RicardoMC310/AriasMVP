13c25031e14cb028ee5e91d7d4d9c655da45eaa9
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 22:15:57 -0300

    feat: implement findUserByEmail in kysely user repository

--- backend/src/modules/user/infrastructure/database/kysely.infra.ts
+++ backend/src/modules/user/infrastructure/database/kysely.infra.ts
@@ -3,6 +3,7 @@ import UserEntity from "../../domain/entity/user.entity.js";
 import IUserRepository from "../../domain/repository/user.repository.js";
 import { DB } from "../../../../platform/database/db.js";
 import { createDatabase } from "../../../../platform/database/kysely.connection.js";
+import UserEntityBuilder from "../../domain/builder/user-entity.builder.js";
 
 export default class KyselyUserRepository implements IUserRepository {
 
@@ -18,6 +19,23 @@ export default class KyselyUserRepository implements IUserRepository {
                 password_hash: userEntity.passwordHash
             })
             .executeTakeFirst();
+    }
+
+    async findUserByEmail(email: string): Promise<UserEntity | null> {
+        const user = await this.db.selectFrom("users")
+            .selectAll()
+            .where("email", "=", email)
+            .executeTakeFirst();
+
+        if (user === undefined)
+            return null;
+
+        return UserEntityBuilder.create()
+            .withId(user.id)
+            .withEmail(user.email)
+            .withUsername(user.name)
+            .withPasswordHash(user.password_hash)
+            .build();
     }
 
 }
