efd344caf4caa06a74ab758a953b5308f19e5c30
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 09:01:13 -0300

    feat: add mailpit container helper for integration tests

---
+++ backend/tests/setup/containers/mailpit.container.ts
@@ -0,0 +1,16 @@
+import { GenericContainer, StartedTestContainer, Wait } from "testcontainers";
+
+export default async function createMailpitContainer() {
+    const container: StartedTestContainer = await new GenericContainer("axllent/mailpit")
+        .withExposedPorts(1025)
+        .withWaitStrategy(
+            Wait.forLogMessage("[http] accessible via http://localhost:8025/")
+        )
+        .start();
+
+    return {
+        container,
+        host: container.getHost(),
+        port: container.getMappedPort(1025)
+    }
+}
