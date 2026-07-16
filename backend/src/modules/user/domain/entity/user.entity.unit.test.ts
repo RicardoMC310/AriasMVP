e21a6587272870e310fc84aaaf00e0f4eaee0c95
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 11:11:16 -0300

    test: add unit tests for user state transitions

--- backend/src/modules/user/domain/entity/user.entity.unit.test.ts
+++ backend/src/modules/user/domain/entity/user.entity.unit.test.ts
@@ -1,6 +1,7 @@
 import UserEntityBuilder from "../builder/user-entity.builder.js";
 import { describe, expect, it } from "@jest/globals";
 import { UserState } from "./user.entity.js";
+import UserCannotTransitionStateException from "../exception/cannot-transition-state.exception.js";
 
 describe("Testes da entidade de usuário", () => {
 
@@ -21,6 +22,66 @@ describe("Testes da entidade de usuário", () => {
         expect(userEntity.username).toBe(username);
         expect(userEntity.passwordHash).toBe(passwordHash);
         expect(userEntity.state).toBe(UserState.ACTIVE);
+    });
+
+    it("Deve permitir transição de não verificado para verificado", () => {
+        const userEntity = UserEntityBuilder.create()
+            .withState(UserState.VERIFICATION_PENDING)
+            .withEmail("ricardo@gmal.com")
+            .withUsername("Ricardo")
+            .build();
+
+        userEntity.verifyEmail();
+
+        expect(userEntity.state).toBe(UserState.ACTIVE);
+    });
+
+    it("Deve bloquear tentar verificar usuário já verificado", () => {
+        const userEntity = UserEntityBuilder.create()
+            .withState(UserState.ACTIVE)
+            .withEmail("ricardo@gmal.com")
+            .withUsername("Ricardo")
+            .build();
+
+        expect(() => userEntity.verifyEmail()).toThrow(UserCannotTransitionStateException);
+
+        expect(userEntity.state).toBe(UserState.ACTIVE);
+    });
+
+    it("Deve bloquear usuário bloqueado de verificar", () => {
+        const userEntity = UserEntityBuilder.create()
+            .withState(UserState.BLOCKED)
+            .withEmail("ricardo@gmal.com")
+            .withUsername("Ricardo")
+            .build();
+
+        expect(() => userEntity.verifyEmail()).toThrow(UserCannotTransitionStateException);
+
+        expect(userEntity.state).toBe(UserState.BLOCKED);
+    })
+
+    it("Deve permitir transição de não verificado para bloqueado", () => {
+        const userEntity = UserEntityBuilder.create()
+            .withState(UserState.VERIFICATION_PENDING)
+            .withEmail("ricardo@gmal.com")
+            .withUsername("Ricardo")
+            .build();
+
+        userEntity.block();
+
+        expect(userEntity.state).toBe(UserState.BLOCKED);
+    });
+
+    it("Deve permitir transição de ativo para bloqueado", () => {
+        const userEntity = UserEntityBuilder.create()
+            .withState(UserState.ACTIVE)
+            .withEmail("ricardo@gmal.com")
+            .withUsername("Ricardo")
+            .build();
+
+        userEntity.block();
+
+        expect(userEntity.state).toBe(UserState.BLOCKED);
     });
 
 });
