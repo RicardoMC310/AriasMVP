7e7f672067040ceedda4bdb46f204039fc006ff4
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-15 23:46:58 -0300

    fix: allow null return in auth user finder port

--- backend/src/modules/auth/application/port/user-finder.port.ts
+++ backend/src/modules/auth/application/port/user-finder.port.ts
@@ -1,5 +1,5 @@
 import UserEntity from "../../../user/domain/entity/user.entity.js";
 
 export default interface IAuthUserFinder {
-    findUserByEmail(email: string): Promise<UserEntity>;
+    findUserByEmail(email: string): Promise<UserEntity | null>;
 }
