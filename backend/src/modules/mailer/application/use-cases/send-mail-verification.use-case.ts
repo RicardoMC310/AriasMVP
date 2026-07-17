5a670d4e9ce05186aa552cdc43d96db476fb40c9
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 21:22:01 -0300

    feat: add send mail verification use case

---
+++ backend/src/modules/mailer/application/use-cases/send-mail-verification.use-case.ts
@@ -0,0 +1,40 @@
+import Email from "../../../../core/domain/vo/email.vo.js";
+import MailEntityBuilder from "../../domain/builder/mail-entity.builder.js";
+import SendEmailVerificationDTO from "../dto/in/send-email-verification.dto.js";
+import IMailerHtmlCompile from "../port/compile-html.port.js";
+import IMailerConfigutation from "../port/mail-configuration.port.js";
+import IMailerTransporter from "../port/mailer.port.js";
+
+export default class SendMailVerificationUseCase {
+
+    constructor(
+        private readonly transporter: IMailerTransporter,
+        private readonly configuration: IMailerConfigutation,
+        private readonly htmlCompile: IMailerHtmlCompile
+    ) { }
+
+    async execute(dto: SendEmailVerificationDTO): Promise<void> {
+        this.validate(dto);
+
+        const bodyBuffer = await this.htmlCompile.compile(
+            "email-verification",
+            {
+                "token": dto.token
+            }
+        );
+
+        const mailEntity = MailEntityBuilder.create()
+            .withFrom(`${this.configuration.name} <${this.configuration.address.toString()}>`)
+            .withTo(dto.email)
+            .withSubject("Email Validation (Arias ERP)")
+            .withBody(bodyBuffer.toString("utf-8"))
+            .build();
+
+        await this.transporter.sendMail(mailEntity);
+    }
+
+    private validate(dto: SendEmailVerificationDTO) {
+        Email.isValid(dto.email);
+    }
+
+}
