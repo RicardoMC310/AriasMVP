ec925f1a3a9968a5d239087a50d6771c0b0309e0
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-17 17:14:34 -0300

    feat: inject ResendEmailVerificationUseCase in email verification controller compositor

--- backend/src/compositor/email-verification/controller/controller.compositor.ts
+++ backend/src/compositor/email-verification/controller/controller.compositor.ts
@@ -1,7 +1,10 @@
 import { Kysely } from "kysely";
 import { DB } from "../../../platform/database/db.js";
 import EmailVericationController from "../../../modules/email-verification/presentation/controller/email-verification.controller.js";
+import resendEmailVerificationUseCaseFactory from "../use-cases/resend-email-verification.compositot.ts/resend-email-verification.compositor.js";
 
 export default function emailVerificationControllerFactory(db: Kysely<DB>) {
-    return new EmailVericationController();
+    return new EmailVericationController(
+        resendEmailVerificationUseCaseFactory(db)
+    );
 }
