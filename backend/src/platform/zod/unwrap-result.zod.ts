0cd104bcf98b1c653ebf2ba659645f491e939a98
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-17 00:03:40 -0300

    chore: rename unwrapZodResult to unwrapResult

--- backend/src/platform/zod/unwrap-result.zod.ts
+++ backend/src/platform/zod/unwrap-result.zod.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 
-export default function unwrapZodResult<T>(result: z.ZodSafeParseResult<T>): T {
+export default function unwrapResult<T>(result: z.ZodSafeParseResult<T>): T {
     if (!result.success) {
         const message = result.error.issues
             .map(issue => `${issue.path} - ${issue.message}`)
