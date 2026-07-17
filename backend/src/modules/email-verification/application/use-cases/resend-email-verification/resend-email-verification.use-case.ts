98dadffd3e92e4a243404d71a4ec672512d1dd69
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-17 17:11:46 -0300

    feat: add ResendEmailVerificationUseCase with observer pattern

---
+++ backend/src/modules/email-verification/application/use-cases/resend-email-verification/resend-email-verification.use-case.ts
@@ -0,0 +1,63 @@
+import Email from "../../../../../core/domain/vo/email.vo.js";
+import EmailVerificationEntityBuilder from "../../../domain/builder/email-verification.builder.js";
+import EmailVerificationNotExistsException from "../../../domain/exception/email-verification-not-exists.exception.js";
+import IEmailVerificationRepository from "../../../domain/repository/email-verification.repository.js";
+import ResendEmailVerificationDTO from "../../dto/in/resend-email-verification/resend-email-verification.dto.js";
+import IEmailVerificationCodeGenerator from "../../port/code-generator.port.js";
+import EmailVerificationObserver from "../../port/mailer-send-observer.port.js";
+
+export default class ResendEmailVerificationUseCase {
+
+    private readonly MILLISECONDS = 1000;
+    private readonly SECONDS = 60;
+    private readonly MINUTES = 30;
+    private readonly TOKEN_LENGTH = 8;
+
+    private readonly observers: EmailVerificationObserver[] = [];
+
+    constructor(
+        private readonly repository: IEmailVerificationRepository,
+        private readonly codeGenerator: IEmailVerificationCodeGenerator
+    ) { }
+
+    async execute(dto: ResendEmailVerificationDTO): Promise<void> {
+        this.validateInput(dto);
+
+        const expiresAt = new Date(
+            Date.now() + this.MILLISECONDS * this.SECONDS * this.MINUTES
+        );
+
+        const { token, hash } = this.codeGenerator.generator(this.TOKEN_LENGTH);
+
+        const emailVerificationEntity = EmailVerificationEntityBuilder.create()
+            .withEmail(dto.email)
+            .withExpiresAt(expiresAt)
+            .withCodeHash(hash)
+            .build();
+
+        const isSuccess = await this.repository.update(emailVerificationEntity);
+
+        if (!isSuccess) {
+            throw new EmailVerificationNotExistsException(dto.email);
+        }
+
+        await this.notifyAll(dto.email, token);
+    }
+
+    registerObserver(observer: EmailVerificationObserver) {
+        this.observers.push(observer);
+    }
+
+    private validateInput(dto: ResendEmailVerificationDTO) {
+        Email.isValid(dto.email);
+    }
+
+    private async notifyAll(email: string, token: string) {
+        await Promise.all([
+            ...this.observers.map(observer => {
+                observer.execute({ email, token });
+            })
+        ]);
+    }
+
+}
