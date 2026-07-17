0bc1c47e208a8deea0275623da222ddb1794f22b
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-07-17 16:56:36 -0300

    feat: refactor api client according cookie session backend

--- app/lib/api/api_client.dart
+++ app/lib/api/api_client.dart
@@ -1,17 +1,39 @@
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/browser.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ApiClient {
-  static const String defaultBaseUrl = String.fromEnvironment(
-    'API_BASE_URL',
-    defaultValue: 'https://localhost:8000',
-  );
+  
 
-  final Dio client = Dio(
-    BaseOptions(
-      baseUrl: defaultBaseUrl,
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
-      headers: {'Content-Type': 'application/json'},
-    ),
-  );
+  static Future<Dio> create() async {
+    final defaultBaseUrl = String.fromEnvironment(
+      'API_BASE_URL',
+      defaultValue: 'https://localhost:8000',
+    );
+
+    final httpAdapter = BrowserHttpClientAdapter();
+    httpAdapter.withCredentials = true;
+
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final cookieJar = PersistCookieJar(
+      storage: FileStorage('${appDocDir.path}/.cookies/')
+    );
+
+    final Dio client = Dio(
+      BaseOptions(
+        baseUrl: defaultBaseUrl,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
+
+    client.httpClientAdapter = httpAdapter;
+
+    client.interceptors.add(CookieManager(cookieJar));
+
+    return client;
+  }
 }
