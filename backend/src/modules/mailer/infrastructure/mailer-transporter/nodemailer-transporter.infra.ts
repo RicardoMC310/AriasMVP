14e936495d9e858f5e7f2fee5f3bd6d37db76cfc
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 21:22:09 -0300

    feat: add nodemailer transporter implementation

---
+++ backend/src/modules/mailer/infrastructure/mailer-transporter/nodemailer-transporter.infra.ts
@@ -0,0 +1,28 @@
+import IMailerTransporter from "../../application/port/mailer.port.js";
+import MailEntity from "../../domain/entities/mail.entity.js";
+import nodemailer from "nodemailer";
+
+export default class NodemailerMailerTransporter implements IMailerTransporter {
+
+    constructor(
+        private readonly transporter: nodemailer.Transporter
+    ) {}
+
+    async sendMail(mailEntity: MailEntity): Promise<void> {
+        this.transporter.sendMail({
+            from: mailEntity.from,
+            to: mailEntity.to,
+            subject: mailEntity.subject,
+            html: mailEntity.body,
+            attachments: [
+                ...mailEntity.attachments.map(attachment => {
+                    return {
+                        filename: attachment.filename,
+                        content: attachment.filename
+                    }
+                })
+            ]
+        });
+    }
+
+}
