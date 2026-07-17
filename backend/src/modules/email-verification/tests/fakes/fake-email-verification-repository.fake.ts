7f238113860368cb90c14e3a92c47ade4ad9d7e3
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-17 19:08:57 -0300

    chore: extract shared test fakes for email verification module

---
+++ backend/src/modules/email-verification/tests/fakes/fake-email-verification-repository.fake.ts
@@ -0,0 +1,32 @@
+import EmailVerificationEntity from "../../domain/entities/email-verification.entity.js";
+import IEmailVerificationRepository from "../../domain/repository/email-verification.repository.js";
+
+export default class TestFakeEmailVerificationRepository implements IEmailVerificationRepository {
+
+    emails: EmailVerificationEntity[] = [];
+    shouldFail = false;
+
+    async save(emailVerificationEntity: EmailVerificationEntity): Promise<void> {
+        if (this.shouldFail) throw new Error("Repository save failed");
+
+        this.emails.push(emailVerificationEntity);
+    }
+
+    injectEntity(entities: EmailVerificationEntity[]) {
+        this.emails = entities;
+    }
+
+    async update(emailVerificationEntity: EmailVerificationEntity): Promise<boolean> {
+        if (this.shouldFail) throw new Error("Repository update failed");
+
+        const index = this.emails.findIndex(email => email.email === emailVerificationEntity.email);
+
+        if (index === -1)
+            return false;
+
+        this.emails[index] = emailVerificationEntity;
+
+        return true;
+    }
+
+}
