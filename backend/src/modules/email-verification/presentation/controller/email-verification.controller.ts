fde8834a8ded2f6a210919ac268e7e6eee64f883
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-16 23:51:35 -0300

    fix: rename email verification controller class

--- backend/src/modules/email-verification/presentation/controller/email-verification.controller.ts
+++ backend/src/modules/email-verification/presentation/controller/email-verification.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import createHttpResponse from "../../../../platform/express/create-response.express.js";
 
-export default class EmailVerificationController {
+export default class EmailVericationController {
+
+    constructor() {}
 
     resend = async (req: Request, res: Response) => {
         const response = createHttpResponse();
