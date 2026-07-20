6fe4faad34d294c089772940f6c60ce885af301c
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-20 07:46:57 -0300

    chore: update InvalidPasswordException to use CategoryError with error code

--- backend/src/modules/user/domain/exception/invalid-password.exception.ts
+++ backend/src/modules/user/domain/exception/invalid-password.exception.ts
@@ -1,9 +1,9 @@
-import DomainException, { DetailError } from "../../../../core/domain/exception/domain.exception.js";
+import DomainException, { CategoryError } from "../../../../core/domain/exception/domain.exception.js";
 
 export default class InvalidPasswordException extends DomainException {
 
     constructor() {
-        super("The password must have at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character", DetailError.VALIDATION);
+        super("The password must have at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character", CategoryError.VALIDATION, "INVALID_FORMAT");
     }
 
 }
