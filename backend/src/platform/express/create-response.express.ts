5af3d6a344eb56dc2338ccc690b93a30082da628
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-20 07:47:31 -0300

    feat: add code and meta fields to HTTP response builder

--- backend/src/platform/express/create-response.express.ts
+++ backend/src/platform/express/create-response.express.ts
@@ -1,13 +1,17 @@
 type HttpResponse = {
     message: string;
     statusCode: number;
-    data: Object
+    data: Object,
+    code: string;
+    meta: Object;
 };
 
-export default function createHttpResponse(message: string = "ok", statusCode: number = 200, data: Object = []): HttpResponse {
+export default function createHttpResponse(message: string = "OK", code: string = "OK", statusCode: number = 200, data: Object = [], meta: Object = []): HttpResponse {
     return {
         message,
-        statusCode,
-        data
+        code,
+        data,
+        meta,
+        statusCode
     }
 }
