ac4563a50e624292a788fc0bf11f7d01e77acc3e
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-20 07:46:32 -0300

    fix: rename unsureNotExpired to ensureNotExpired in email verification entity

--- backend/src/modules/email-verification/domain/entities/email-verification.entity.ts
+++ backend/src/modules/email-verification/domain/entities/email-verification.entity.ts
@@ -60,7 +60,7 @@ export default class EmailVerificationEntity {
         this._attempts++;
     }
 
-    unsureNotExpired() {
+    ensureNotExpired() {
         if (this._expiresAt.getTime() < Date.now()) 
             throw new ExpiredEmailVerificationException();
     }
