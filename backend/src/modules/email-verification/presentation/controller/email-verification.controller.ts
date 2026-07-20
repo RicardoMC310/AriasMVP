7c9e16d1756de5b183250347a1df88314628c930
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-20 07:47:29 -0300

    feat: add verify endpoint to EmailVerificationController

--- backend/src/modules/email-verification/presentation/controller/email-verification.controller.ts
+++ backend/src/modules/email-verification/presentation/controller/email-verification.controller.ts
@@ -3,11 +3,14 @@ import createHttpResponse from "../../../../platform/express/create-response.exp
 import { ResendEmailVerificationDTOSchema } from "../../application/dto/in/resend-email-verification/resend-email-verification.dto.js";
 import unwrapResult from "../../../../platform/zod/unwrap-result.zod.js";
 import ResendEmailVerificationUseCase from "../../application/use-cases/resend-email-verification/resend-email-verification.use-case.js";
+import { VerifyEmailVerificationRequestDTOSchema } from "../../application/dto/in/verify-email-verifcation/verify-email-verification.dto.js";
+import VerifyEmailVerificationUseCase from "../../application/use-cases/verify-email-verification/verify-email-verification.use-case.js";
 
 export default class EmailVericationController {
 
     constructor(
-        private readonly resendEmailVerificationUseCase: ResendEmailVerificationUseCase
+        private readonly resendEmailVerificationUseCase: ResendEmailVerificationUseCase,
+        private readonly verifyEmailVerificarionUseCase: VerifyEmailVerificationUseCase
     ) {}
 
     resend = async (req: Request, res: Response) => {
@@ -16,7 +19,17 @@ export default class EmailVericationController {
 
         await this.resendEmailVerificationUseCase.execute(body);
 
-        const response = createHttpResponse("Check your email inbox");
+        const response = createHttpResponse("Check your email inbox", "SUCCESSFULY");
+        res.status(response.statusCode).json(response);
+    }
+
+    verify = async (req: Request, res: Response) => {
+        const bodyRaw = VerifyEmailVerificationRequestDTOSchema.safeParse(req.body);
+        const body = unwrapResult(bodyRaw);
+
+        await this.verifyEmailVerificarionUseCase.execute(body);
+
+        const response = createHttpResponse("User Verified", "SUCCESSFULY");
         res.status(response.statusCode).json(response);
     }
 
