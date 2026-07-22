42cf21be564f272a735e85224be838be71ed6292
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-07-22 13:44:14 -0300

    feat: add findUserById and update methods to user repository

--- backend/src/modules/user/domain/repository/user.repository.ts
+++ backend/src/modules/user/domain/repository/user.repository.ts
@@ -3,4 +3,6 @@ import UserEntity from "../entity/user.entity.js";
 export default interface IUserRepository {
     save(userEntity: UserEntity): Promise<void>;
     findUserByEmail(email: string): Promise<UserEntity | null>;
+    findUserById(id: string): Promise<UserEntity | null>;
+    update(userEntity: UserEntity): Promise<void>;
 }
